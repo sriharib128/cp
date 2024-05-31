a7084b195a3d51cf3f8564387599973318453264
sriharib128 <sriharib128@gmail.com>
2024-05-31 11:22:24 +0530

    LRU

--- saved/LRU_dll.cpp
+++ saved/LRU_dll.cpp
@@ -68,3 +68,9 @@ public:
         }
     }
 };
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
